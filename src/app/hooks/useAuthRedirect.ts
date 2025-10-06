87f2d919551fe821f3b28c679ecd608885e0a652
marcospedroweb <king.frontweb@outlook.com>
2025-10-06 19:21:19 -0300

    feat: redirect user if has token

---
+++ src/app/hooks/useAuthRedirect.ts
@@ -0,0 +1,25 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { useRouter, usePathname } from "next/navigation"
+export function useAuthRedirect() {
+  const router = useRouter();
+  const pathname = usePathname();
+  const [checking, setChecking] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const publicPaths = ["/login", "/signup"];
+    const isPublicPath = publicPaths.includes(pathname);
+
+    if (!token && !isPublicPath) {
+      router.replace("/login");
+    } else if (token && isPublicPath) {
+      router.replace("/products");
+    } else {
+      setChecking(false);
+    }
+  }, [pathname, router]);
+
+  return checking;
+}
