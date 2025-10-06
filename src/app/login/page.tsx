87268bd91350b397aa6cbc0bdecef24f25df2f97
marcospedroweb <king.frontweb@outlook.com>
2025-10-06 19:22:50 -0300

    feat: complete page UI with functional login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,101 @@
+'use client';
+
+import { useState } from 'react';
+import Input from '../components/Input';
+import { FaUnlockAlt, FaUser } from 'react-icons/fa';
+import Button from '../components/Button';
+import { useUserStore } from '@/lib/store';
+import { useMutation } from '@tanstack/react-query';
+import api from '@/lib/axios';
+import { useRouter } from 'next/navigation';
+import { useAuthRedirect } from '../hooks/useAuthRedirect';
+
+export default function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [stayLogged, setStayLogged] = useState(false);
+  const { login } = useUserStore();
+  const navigate = useRouter();
+
+  useAuthRedirect();
+
+  const mutation = useMutation({
+    mutationFn: async () => {
+      const res = await api.post('/auth/login', {
+        username,
+        password,
+      });
+      return res.data;
+    },
+    onSuccess: (data) => {
+      if (data.status === 1) {
+        login(data.dados_usuario, data.token_de_acesso);
+        if (stayLogged) {
+          localStorage.setItem('token', data.token_de_acesso);
+        }
+        navigate.push('/products');
+      } else {
+        alert(data.message);
+      }
+    },
+  });
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    mutation.mutate();
+  };
+
+  return (
+    <form
+      className="w-full h-screen bg-cover bg-no-repeat relative"
+      style={{
+        backgroundImage: "url('/assets/login_banner.jpg')",
+        backgroundPosition: 'center',
+      }}
+      onSubmit={handleSubmit}
+    >
+      <div className="relative z-10 flex flex-col items-center justify-center min-h-screen max-w-xl mx-auto">
+        <div className="w-full text-center">
+          <h1 className="text-[#80bc04] font-bold text-3xl mb-8">
+            Bem-vindo a Innovation Brindes
+          </h1>
+          <div className="bg-[#80bc04] p-6 rounded-2xl py-24">
+            <div className="max-w-[60%] mx-auto">
+              <Input
+                text={'Usuário'}
+                icon={<FaUser className="text-gray-600" />}
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+              <Input
+                text={'Senha'}
+                type="password"
+                icon={<FaUnlockAlt className="text-gray-600" />}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              <div className="flex justify-between items-center w-[90%] mx-auto">
+                <div>
+                  <input
+                    type="checkbox"
+                    id="scales"
+                    name="scales"
+                    checked={stayLogged}
+                    onChange={() => setStayLogged((oldState) => !oldState)}
+                  />
+                  <label htmlFor="scales" className="ms-2">
+                    <small>Manter logado</small>
+                  </label>
+                </div>
+                <small>Esqueceu a password?</small>
+              </div>
+              <Button text={'Login'} className="mt-8" type="submit" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+}
