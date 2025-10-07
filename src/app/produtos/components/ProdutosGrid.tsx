f40cdf9769e3922a923ec4e25b14be62bd7005de
marcospedroweb <king.frontweb@outlook.com>
2025-10-07 14:19:20 -0300

    feat: products with debounce in search / filter products component

--- src/app/produtos/components/ProdutosGrid.tsx
+++ src/app/produtos/components/ProdutosGrid.tsx
@@ -6,12 +6,25 @@ import { useQuery } from '@tanstack/react-query';
 import { toast } from 'react-toastify';
 import ProdutoCard from './ProdutoCard';
 import ProdutoCardSkeleton from './ProdutoCardSkeleton';
+import Filter from './Filter';
+import { useEffect, useState } from 'react';
+import { useDebounce } from '@/app/hooks/useDebounce';
 
 const ProdutosGrid = () => {
+  const [searchValue, setSearchValue] = useState('');
+  const debouncedSearch = useDebounce(searchValue, 500);
+
   const { data: produtos = [], isLoading } = useQuery<Produto[], Error>({
-    queryKey: ['produtos'],
+    queryKey: ['produtos', debouncedSearch],
     queryFn: async () => {
       try {
+        if (debouncedSearch.trim() !== '') {
+          const res = await api.post('/innova-dinamica/produtos', {
+            search: debouncedSearch,
+          });
+          return res.data;
+        }
+
         const res = await api.get('/innova-dinamica/produtos');
         return res.data;
       } catch (error: unknown) {
@@ -29,7 +42,11 @@ const ProdutosGrid = () => {
 
   return (
     <div className="max-w-10/12 mx-auto">
-      <div>filters</div>
+      <Filter search={searchValue} setSearch={setSearchValue} />
+
+      <p className="text-end text-xl mt-2 mb-4 font-medium">
+        {produtos.length} produtos encontrados
+      </p>
       <div className="flex justify-between items-start flex-wrap gap-5">
         {isLoading
           ? Array.from({ length: 6 }).map((_, i) => (
