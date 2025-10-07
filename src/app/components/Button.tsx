789e34342a80539c70c9cc4012fb1ae749c4d484
marcospedroweb <king.frontweb@outlook.com>
2025-10-07 09:46:13 -0300

    refactor: loading button

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -3,13 +3,22 @@ import React, { ButtonHTMLAttributes } from 'react';
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   text: string;
   className?: string;
+  loading: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ text, className = '', ...props }) => {
+const Button: React.FC<ButtonProps> = ({
+  text,
+  className = '',
+  loading,
+  ...props
+}) => {
   return (
     <button
       {...props}
-      className={`px-16 py-4 bg-white hover:bg-gray-100 cursor-pointer rounded-full disabled:opacity-50 font-bold text-gray-800 ${className}`}
+      className={`px-16 py-4 bg-white hover:bg-gray-100 ${
+        loading ? 'cursor-progress' : 'cursor-pointer'
+      } rounded-full disabled:opacity-50 font-bold text-gray-800 ${className}`}
+      disabled={loading}
     >
       {text}
     </button>
