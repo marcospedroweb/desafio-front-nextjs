51bf4b9ba54f8140b7e82dcb5819255c10605db7
marcospedroweb <king.frontweb@outlook.com>
2025-10-07 16:47:41 -0300

    feat: component saveIcon with icon

---
+++ src/app/produtos/components/ProdutoSaveIcon.tsx
@@ -0,0 +1,28 @@
+import { FaBookmark, FaRegBookmark } from 'react-icons/fa';
+
+interface ProdutoSaveIconProps {
+  produtoCode: string;
+  toggleSaveProduct: (codigo: string) => void;
+  isSaved: boolean;
+}
+
+const ProdutoSaveIcon = ({
+  produtoCode,
+  toggleSaveProduct,
+  isSaved,
+}: ProdutoSaveIconProps) => {
+  return (
+    <div
+      className="bg-[#f9f9f9] top-2 left-2 absolute p-0.5 z-10 cursor-pointer"
+      onClick={() => toggleSaveProduct(produtoCode)}
+    >
+      {isSaved ? (
+        <FaBookmark className="text-xl" />
+      ) : (
+        <FaRegBookmark className="text-xl" />
+      )}
+    </div>
+  );
+};
+
+export default ProdutoSaveIcon;
