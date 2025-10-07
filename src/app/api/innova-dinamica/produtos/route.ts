67e77e36279993d56a962f5db6e69f7e56b1a8f1
marcospedroweb <king.frontweb@outlook.com>
2025-10-07 14:13:57 -0300

    feat: route post to get products by code or name

--- src/app/api/innova-dinamica/produtos/route.ts
+++ src/app/api/innova-dinamica/produtos/route.ts
@@ -31,3 +31,44 @@ export async function GET(req: NextRequest) {
     )
   }
 }
+
+export async function POST(req: NextRequest) {
+  try {
+    const { search } = await req.json()
+    const authHeader = req.headers.get('authorization');
+    const token = authHeader?.split(' ')[1];
+    const codigoProduto = Number(search) || null;
+
+    if (!token) {
+      return NextResponse.json({ status: 0, message: 'Não autorizado' }, { status: 401 });
+    }
+
+    console.log(codigoProduto)
+    console.log(codigoProduto)
+    console.log(codigoProduto)
+    console.log(codigoProduto)
+
+    const { data }: { data: Produto[] } = await axios.post('https://apihomolog.innovationbrindes.com.br/api/innova-dinamica/produtos/listar', codigoProduto ? {
+      codigo_produto: codigoProduto
+    } : {
+      nome_produto: search
+    }, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+
+    return NextResponse.json(data)
+  } catch (error: unknown) {
+    let message = 'Erro desconhecido';
+
+    if (error instanceof Error) {
+      message = error.message;
+    }
+
+    return NextResponse.json(
+      { status: 0, message: message },
+      { status: 500 }
+    )
+  }
+}
