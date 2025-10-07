55993ee686644e0ed2bc6fd506fa869862157cf1
marcospedroweb <king.frontweb@outlook.com>
2025-10-07 11:58:25 -0300

    refactor: request folders changed

---
+++ src/app/api/innova-dinamica/produtos/route.ts
@@ -0,0 +1,35 @@
+import type { Produto } from '@/lib/store';
+import axios from 'axios'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(req: NextRequest) {
+  try {
+    const authHeader = req.headers.get('authorization');
+    const token = authHeader?.split(' ')[1];
+    console.log(authHeader)
+    console.log(token)
+
+    if (!token) {
+      return NextResponse.json({ status: 0, message: 'Não autorizado' }, { status: 401 });
+    }
+
+    const { data }: { data: Produto[] } = await axios.get('https://apihomolog.innovationbrindes.com.br/api/innova-dinamica/produtos/listar', {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+
+    return NextResponse.json(data)
+  } catch (error: unknown) {
+    let message = 'Erro desconhecido';
+
+    if (error instanceof Error) {
+      message = error.message;
+    }
+
+    return NextResponse.json(
+      { status: 0, message: message },
+      { status: 500 }
+    )
+  }
+}
